e528803570edb45d352502c2c288e8e21820c181
Mwakima Matano <Mwakimamatano@gmail.com>
2017-07-01 13:53:21 +0530

    first commit

---
+++ Portfolio Page/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#twitterBtn").css("background-color","#2CAAE1").css("color","#fff");
+	$("#facebookBtn").css("background-color","#3B579D").css("color","#fff");
+	$("#linkedInBtn").css("background-color","#007BB6").css("color","#fff");
+	$("#githubBtn").css("background-color","#000").css("color","#fff");
+	$("#gmailBtn").css("background-color","#F32C26").css("color","#fff");
+	$("#freeCodeBtn").css("background-color","#006400").css("color","#fff");
+});
